2e9f34400df30579ad28372496e4f8336be8e130
F-Dreiling <drooklyn@gmail.com>
2026-03-28 16:58:52 +0100

    example.config.php

---
+++ config/example.config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'app' => [
+        'name' => 'GeoTourist',
+        'base_path' => '/geotourist',
+    ],
+    'backend_url' => 'http://localhost:8080/locations',
+    'maps_api_key' => 'YOUR_API_KEY',
+];
+
+?>
